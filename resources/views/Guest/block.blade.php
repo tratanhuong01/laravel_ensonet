e901a41d686c413dc980c345cf3b86c891429a06
tratanhuong01 <48879567+tratanhuong01@users.noreply.github.com>
2021-04-15 23:21:32 +0700

    Update and rename Block.blade.php to block.blade.php

--- resources/views/Guest/block.blade.php
+++ resources/views/Guest/block.blade.php
@@ -8,17 +8,20 @@
 <body class="bg-gray-200">
     <div class="w-1/2 mx-auto p-3 bg-white absolute top-1/3 left-1/2 rounded-lg" style="transform: translate(-50%,-50%);">
         <div class="w-full py-2">
-            <p class="font-bold text-xm" style="font-family: system-ui;">Tài khoản của bạn đã bị khóa tạm thời </p>
+            <p class="font-bold text-xm" style="font-family: system-ui;">
+                Tài khoản của bạn đã bị khóa tạm thời
+            </p>
         </div>
         <hr>
         <div class="w-full py-2">
             <p class="pt-2">Chúng tôi phát hiện rằng tài khoản của bạn có những thay đổi bất thường vui lòng cho chúng
-                tôi biết bạn đang gặp sự cố gì . Vui lòng bấm tiếp tục để mở tài khoản vui lòng kiểm tra
+                tôi biết bạn đang gặp sự cố gì. Vui lòng bấm tiếp tục để mở tài khoản vui lòng kiểm tra
                 các hoạt động gần đây của bạn.
             </p>
             <p class="pt-2">
                 Có vẻ như đã có người xâm nhập vào tài khoản của bạn , có thể nhập vào một website được thiết kế
-                giống <b>&nbsp;Ensonet</b>. Loại tấn công này là lừa đảo . <a href="" class="text-blue-600">Tìm hiểu thêm</a>
+                giống <b>&nbsp;
+                    Ensonet</b>. Loại tấn công này là lừa đảo . <a href="" class="text-blue-600">Tìm hiểu thêm</a>
             </p>
             <p class="pt-2">Qua vài bước bạn có thể mở lại tài khoản của mình . Hãy nhớ cẩn trọng với tài khoản của mình , không
                 cung cấp bất kì thông tin gì cho người khác .
